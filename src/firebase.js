b64aa0e3fdec5de5f312992046006a61e5117cf5
Omar Bazeed <omarbazeed@gmail.com>
2022-11-08 21:27:59 +0200

    great update

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
+//     appId: process.env.REACT_APP_FIREBASE_APPID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+// };
 
 const firebaseConfig = {
     apiKey: "AIzaSyDGv1f1hs9iqIkb3MMeYdsKggFvBuFQr54",
